ad55e049b631abef5f1d4eb39c1e76cce6a51415
Frederick Noon <fnoon@proton.me>
2024-06-14 12:50:03 -0700

    Make input slice mutable, avoiding a copy

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-fn median(a: &[f32]) -> Option<f32> {
+fn median(a: &mut [f32]) -> Option<f32> {
     match a.len() {
         0 => None, // empty vector
         n => {
-            let mut a = Vec::from(a); // create a mutable vector for sorting
             a.sort_by(|a, b| a.partial_cmp(b).unwrap()); // panic on NaN
             let mid = n / 2; // find the middle index
             match (n % 2) == 0 {
@@ -21,23 +20,23 @@ fn median(a: &[f32]) -> Option<f32> {
 }
 
 fn main() {
-    let answer = median(&[1.0, 2.0, 5.0]);
+    let answer = median(&mut [1.0, 2.0, 5.0]);
 
     println!("median([1,2,5]) = {:?}", answer);
 }
 
 #[test]
 fn empty_list() {
-    let input = vec![];
+    let mut input = vec![];
     let expected_output = None;
-    let actual_output = median(&input);
+    let actual_output = median(&mut input);
     assert_eq!(actual_output, expected_output);
 }
 
 #[test]
 fn sorted_list() {
     // Experiment with defining a slice for this test:
-    let input = &[1.0, 4.0, 5.0];
+    let input = &mut [1.0, 4.0, 5.0];
     let expected_output = Some(4.0);
     let actual_output = median(input);
     assert_eq!(actual_output, expected_output);
@@ -45,16 +44,16 @@ fn sorted_list() {
 
 #[test]
 fn even_length() {
-    let input = vec![1.0, 3.0, 5.0, 6.0];
+    let mut input = vec![1.0, 3.0, 5.0, 6.0];
     let expected_output = Some(4.0);
-    let actual_output = median(&input);
+    let actual_output = median(&mut input);
     assert_eq!(actual_output, expected_output);
 }
 
 #[test]
 fn unsorted_list() {
-    let input = vec![1.0, 5.0, 2.0];
+    let mut input = vec![1.0, 5.0, 2.0];
     let expected_output = Some(2.0);
-    let actual_output = median(&input);
+    let actual_output = median(&mut input);
     assert_eq!(actual_output, expected_output);
 }
